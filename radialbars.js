35c1eb67c171c90d7e8b1f250f4c5da7c48c54c8
cat ChrisTrees <cat@catmini.cat9.private>
2019-07-08 21:57:35 -0500

    basic working game

--- radialbars.js
+++ radialbars.js
@@ -1,34 +1,127 @@
-var rbObj = {
+let rbObj = {
   mainObject: null,
   mainSelect: '.radial-bars',
   viewBox: '0 0 300 300',
+  score: {
+    selectorByID: "Score",
+    current: 0,
+    maxPlayer: 0,
+  },
   hub: {
     classname: 'hub',
     type: 'circle',
-    attr: [ 
-      ['r', '50'], 
-      ['fill', 'white'],
-      ['stroke', 'green'],
-      ['stroke-width', '3']
-    ]
+    center: '(150,150)',
+    text: 'Click to Start',
+    attr: { 
+      r: 50, 
+      fill: 'red',
+      stroke: 'green',
+      strokewidth: 3
+    }
   },
-};
-document.addEventListener('DOMContentLoaded', function(event) {
-  // verify d3 is an object in the dom
-  if (typeof d3 == 'object') {
+  bars: {
+    width: 20,
+    height: 100,
+    count: 1,
+    timeout: 10000,
+    pntValAttr: "pointValue",
+    barObjectID: "rect.bar",
+    bar: {
+      durationTime:2000,
+      delayTime:1000,
+      degrees: 360,
+      pointValue: 5,
+    }
+  },
+  makeBoard() {
+    d3.select(rbObj.mainSelect).selectAll('svg').remove();
     rbObj.main = d3.select(rbObj.mainSelect);
     rbObj.main.append('svg')
-      .attr('viewBox', rbObj.viewBox)
+      .attr('viewBox', rbObj.viewBox);
+  },
+  makeHub() {
+    d3.select('.hub').remove();
+    d3.select('text').remove();
+    d3.select('svg')
       .append('circle').classed('hub', true)
-      .attr('r', '50')
-      .attr('fill', 'red')
-      .attr('stroke', 'green')
-      .attr('stroke-width', '3');
+      .attr('r', rbObj.hub.attr.r)
+      .attr('fill', rbObj.hub.attr.fill)
+      .attr('stroke', rbObj.hub.attr.stroke)
+      .attr('stroke-width', rbObj.hub.attr.strokewidth)
+      .attr('transform', 'translate'+rbObj.hub.center);
+    d3.select('svg')
+      .append('text')
+      .text(rbObj.score.current)
+      .attr('transform', 'translate'+rbObj.hub.center);
+  },
+  manageHubClick() {
+    d3.select(this).remove();
+    //console.log(d3.event);
+    //alert(d3.event);
+    rbObj.makeBar();
+    rbObj.makeHub();
+  },
+  makeBar() {
+    var degrees = Math.floor(Math.random() * 360);
+    d3.select("svg")
+    .append("rect")
+    .classed("bar", true)
+    .attr("height", 10)
+    .attr("width", rbObj.bars.width)
+    .attr("rx",rbObj.bars.width/2)
+    .attr("ry",rbObj.bars.width/2)
+    .attr("x", -rbObj.bars.width/2)
+    .attr("y", -rbObj.bars.width/2)
+    .attr(rbObj.bars.pntValAttr, rbObj.bars.bar.pointValue)
+    .attr('transform', 'translate'+rbObj.hub.center+'rotate('+degrees+')')
+    .on("click", rbObj.manageBarClick);
+    d3.selectAll("rect")
+    .attr("height",10)
+    .style("fill","blue")
+    .transition()
+    .attr("height",rbObj.bars.height)
+    .style("fill","green")
+    .duration(rbObj.bars.bar.durationTime)
+    .transition()
+    .attr("height",10)
+    .style("fill","green")
+    .delay(rbObj.bars.bar.delayTime).duration(rbObj.bars.bar.durationTime);
+    barTimer = d3.timer(function(duration) { //console.log(duration);
+      if (duration > rbObj.bars.bar.durationTime)  {
+        rbObj.endGameAnim();
+        barTimer.stop();
+      }
+    }, 1000);
+  },
+  manageBarClick() {
+    barTimer.stop();
+    rbObj.updateScore(d3.select(this).attr(rbObj.bars.pntValAttr));
+    d3.select(this)
+     .attr("height", 10)
+     .transition()
+     .on('end', function() {  
+       d3.select(this).remove();  
+       rbObj.makeBar();
+       rbObj.makeHub();
+      });
+  },
+  updateScore(value) {
+    rbObj.score.current = rbObj.score.current + parseInt(value);
+    d3.select('.score').text(rbObj.score.current);
+    d3.select('text').text("+ "+value);
+  },
+  endGameAnim() {
+    d3.select('text').text("Game Over");
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  if (typeof d3 == 'object') {//verify d3 is an object in the dom
+    rbObj.makeBoard();
+    rbObj.makeHub();
+    d3.select('.hub').on('click', rbObj.manageHubClick);
+    d3.select('text').text(rbObj.hub.text);
   } else {
-    alert('D3.js object not found');
+    alert('Error: d3 not found');
   }
 });
-
-function startGame(){
-  alert('play');
-}
